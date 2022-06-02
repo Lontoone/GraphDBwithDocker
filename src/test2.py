94cdb5da9e9e85debe1a2d2489e7d077c34046e7
cingjia <news2000tw@gmail.com>
2022-06-02 16:49:34 +0800

    update node

--- src/test2.py
+++
@@ -1,9 +0,0 @@
-from random import sample
-import spacy
-
-text="It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming."
-nlp =spacy.load("en_core_web_sm")
-doc = nlp(text)
-
-for token in doc:
-    print(token.text , token.lemma_ ,token.pos_,token.tag_ , token.dep_,token.shape_,token.is_stop)
